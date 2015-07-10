63126ee3db3efc4d9952a0249595bbabf1b994f0
RyanOConnor <RyanMOConnor@lewisu.edu>
2015-07-10 03:04:55 -0500

    Update PeopleMapper.java

--- src/main/java/restapi/PeopleMapper.java
+++ src/main/java/restapi/PeopleMapper.java
@@ -8,7 +8,7 @@ import org.springframework.jdbc.core.RowMapper;
 public class PeopleMapper implements RowMapper<People> {
 	public People mapRow(ResultSet rs, int rowNum) throws SQLException {
 		return new People(rs.getInt("id"), 
-		   	              rs.getString("firstname"), 
-					      rs.getString("lastname"));
+		   	          rs.getString("firstname"), 
+				  rs.getString("lastname"));
 	}
 }
